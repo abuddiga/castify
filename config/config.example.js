efbacf8c4d2931d2228df0405b0108564412d7b8
Akshay Buddiga <abuddiga@stanford.edu>
2016-02-15 11:44:12 -0800

    Skeleton server

---
+++ config/config.example.js
@@ -0,0 +1,7 @@
+// Enter your Spotify authorization details below and uncomment:
+//
+// module.exports = {
+// 	'client_id': 'CLIENT_ID',
+// 	'client_secret': 'CLIENT_SECRET',
+// 	'redirect_uri': 'YOUR_CALLBACK_URL'
+// };
---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(8888, function () {
+  console.log('Example app listening on port 8888!');
+});
