c2a06fb67721f25e3d58956d3ffacc264251aa91
Akshay Buddiga <abuddiga@stanford.edu>
2016-02-15 16:08:03 -0800

    Add playlist api access

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email';
+  var scope = 'user-read-private user-read-email playlist-read-private playlist-read-collaborative';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
