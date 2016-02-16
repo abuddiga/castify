8fd3bf144493487fc953f61d3e5fc813c0607c40
Akshay Buddiga <abuddiga@stanford.edu>
2016-02-15 19:03:44 -0800

    Begin YT API request implementation

--- public/app/playlist/playlist.js
+++ public/app/playlist/playlist.js
@@ -1,6 +1,6 @@
 angular.module('castify.playlist', [])
 
-.controller('PlaylistController', function ($scope, $location, Playlist) {
+.controller('PlaylistController', function ($scope, $location, Playlist, Video) {
   $scope.data = {
     playlists: [],
     currentPlaylist: {},
@@ -12,20 +12,33 @@ angular.module('castify.playlist', [])
     Playlist.getPlaylists()
     .then(function (playlists) {
       $scope.data.playlists = playlists.data.items;
-      selectPlaylist(0);
+      $scope.selectPlaylist(0);
     })
     .catch(function (error) {
       console.error(error);
     });
   };
 
-  var selectPlaylist = function(index) {
+  $scope.selectPlaylist = function(index) {
     $scope.data.currentPlaylist = $scope.data.playlists[index];
 
     Playlist.getSongList($scope.data.currentPlaylist)
     .then(function (songs) {
-      console.log(songs.data.items);
       $scope.data.songList = songs.data.items;
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  };
+
+  $scope.selectSong = function(index) {
+    $scope.data.currentSong = $scope.data.songList[index];
+    
+    Video.getVideo($scope.data.currentSong)
+    .then(function (song) {
+      console.log('got song');
+      console.log(song);
+      // Video.playVideo(song);
     })
     .catch(function (error) {
       console.error(error);
